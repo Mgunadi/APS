bccc5f38979842597b0cb4864fed845594e006b5
Mgunadi <mgun_27@hotmail.com>
2019-03-28 14:02:11 +1100

    First practice with git

---
+++ Practical_notesb/src/tutes/Account.java
@@ -0,0 +1,26 @@
+package tutes;
+
+public class Account {
+
+	private String name; private String ID;
+	public double balance;
+	
+	public void Account(String name, String ID, double balance) {
+		name = name;
+		ID = ID;
+		balance = balance;
+	}
+	
+	public void getBalance() {
+		return balance;
+	}
+	
+	public void withdraw(double amt) {
+		balance -= amt;
+	}
+	
+	public void deposit(double amt) {
+		balance += amt; }
+	}
+
+}
---
+++ Practical_notesb/src/tutes/TestAcct.java
@@ -0,0 +1,10 @@
+package tutes;
+
+public class TestAcct {
+
+	public static void main(String[] args) {
+		System.out.println("Hi. How are you?");
+
+	}
+
+}
