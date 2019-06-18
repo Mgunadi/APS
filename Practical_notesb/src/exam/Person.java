1880edce5578b73630ecfedf5cceae5806a45a0a
Mgunadi <mgun_27@hotmail.com>
2019-06-18 17:42:27 +1000

    Exam

---
+++ Practical_notesb/src/exam/Person.java
@@ -0,0 +1,17 @@
+package exam;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Person {
+
+
+
+public static void main(String[] args) {
+	
+	ArrayList<Person> sentient = new ArrayList<>();
+	sentient.add(new Man());
+	sentient.add(new Woman());
+}
+
+}
